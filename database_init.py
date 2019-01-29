e3e3c61d779c0e6b802c7abd348133fbec69fb03
KF3527 <kf3527@att.com>
2019-01-28 18:30:37 -0600

    updated README

---
+++ database_init.py
@@ -0,0 +1,110 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from sqlalchemy_utils import database_exists, drop_database, create_database
+
+from database_setup import Category, CategoryItem, User, Base
+
+
+def database_init():
+
+
+# Clear database
+	engine = create_engine('sqlite:///itemcatalog.db?check_same_thread=False')
+	Base.metadata.drop_all(engine)
+	Base.metadata.create_all(engine)
+
+# Bind the engine to the metadata of the Base class so that the
+# declaratives can be accessed through a DBSession instance
+	Base.metadata.bind = engine
+
+	DBSession = sessionmaker(bind=engine)
+
+# A DBSession() instance establishes all conversations with the database
+# and represents a "staging zone" for all the objects loaded into the
+# database session object. Any change made against the objects in the
+# session won't be persisted into the database until you call
+# session.commit(). If you're not happy about the changes, you can
+# revert all of them back to the last commit by calling
+# session.rollback()
+	session = DBSession()
+
+# Create dummy user
+	user1 = User(name="Karen Fan",
+              email="fancp@yahoo.com")
+	session.add(user1)
+	session.commit()
+
+# Items for Strings
+	category1 = Category(name="Meat", user_id=1)
+
+	session.add(category1)
+	session.commit()
+
+	item1 = CategoryItem(name="pork", user_id=1, description="Pork is an excellent source of protein", category=category1)
+	session.add(item1)
+	session.commit()
+
+	item2 = CategoryItem(name="beef", user_id=1,  description="Beef is a great source of 10 essential nutrients including protein", category=category1)
+
+	session.add(item2)
+	session.commit()
+
+	item3 = CategoryItem(name="chicken", user_id=1,  description="Skinless white meat chicken breast is the lowest in fat and calories.", category=category1)
+
+	session.add(item3)
+	session.commit()
+
+# Items for Woodwinds
+	category2 = Category(name="Grain", user_id=1)
+
+	session.add(category2)
+	session.commit()
+
+	item1 = CategoryItem(name="bread", user_id=1, description="Bread made with sprouted grains is a good option", category=category2)
+
+	session.add(item1)
+	session.commit()
+
+	item2 = CategoryItem(name="pasta", user_id=1,  description="Pasta is a fat-free, low sodium food that can fit right in with a weight loss or management plan.", category=category2)
+
+	session.add(item2)
+	session.commit()
+
+	item3 = CategoryItem(name="rice", user_id=1, description="Brown rice contains a fair amount of fiber (1.8%), while white rice is very low in fiber (0.3%)", category=category2)
+
+	session.add(item3)
+	session.commit()
+
+# Items for Percussion
+	category3 = Category(name="Dairyfood", user_id=1)
+
+	session.add(category3)
+	session.commit()
+
+	item1 = CategoryItem(name="milk", user_id=1, description="Milk Is the Top-Source of Calcium in Americans' Diets", category=category3)
+
+	session.add(item1)
+	session.commit()
+
+	item2 = CategoryItem(name="cheese", user_id=1, description="Cheese contains a host of nutrients like calcium, protein, phosphorus, zinc, vitamin A and vitamin B12.", category=category3)
+
+	session.add(item2)
+	session.commit()
+
+	item3 = CategoryItem(name="yogurt", user_id=1, description="Yogurt (also spelled yoghurt), is one of the most popular fermented dairy products in the world, made by adding live bacteria to milk", category=category3)
+
+	session.add(item3)
+	session.commit()
+
+# Items for Brass
+	category4 = Category(name="Vegetables", user_id=1)
+
+	session.add(category4)
+	session.commit()
+	categories = session.query(Category).all()
+	for category in categories:
+		print ("Category: " + category.name)
+
+if __name__ == '__main__':
+	database_init()
--- database_setup.py
+++ database_setup.py
@@ -1,51 +1,55 @@
+import os, sys
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
- 
+
 Base = declarative_base()
 
-class Restaurant(Base):
-    __tablename__ = 'restaurant'
-   
+class User(Base):
+    __tablename__ = 'user'
+
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+
+
+class Category(Base):
+    __tablename__ = 'category'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(80), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
     @property
     def serialize(self):
-       """Return object data in easily serializeable format"""
-       return {
-           'name'         : self.name,
-           'id'           : self.id,
-       }
- 
-class MenuItem(Base):
-    __tablename__ = 'menu_item'
+        """Return object data in easily serializeable format"""
+        return {
+            'id': self.id,
+            'name': self.name,
+        }
 
 
-    name =Column(String(80), nullable = False)
-    id = Column(Integer, primary_key = True)
+class CategoryItem(Base):
+    __tablename__ = 'category_item'
+
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
     description = Column(String(250))
-    price = Column(String(8))
-    course = Column(String(250))
-    restaurant_id = Column(Integer,ForeignKey('restaurant.id'))
-    restaurant = relationship(Restaurant)
-
-
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+    category_id = Column(Integer, ForeignKey('category.id'))
+    category = relationship(Category)
     @property
     def serialize(self):
-       """Return object data in easily serializeable format"""
-       return {
-           'name'         : self.name,
-           'description'         : self.description,
-           'id'         : self.id,
-           'price'         : self.price,
-           'course'         : self.course,
-       }
+        """Return object data in easily serializeable format"""
+        return {
+            'category': self.category.name,
+            'description': self.description,
+            'name': self.name,
+        }
 
 
-
-engine = create_engine('sqlite:///restaurantmenu.db')
- 
-
+engine = create_engine('sqlite:///itemcatalog.db?check_same_thread=False')
 Base.metadata.create_all(engine)
